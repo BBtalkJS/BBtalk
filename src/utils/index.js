240c5137971e7c8d7ad71ed5cd37b6909b9b4bbb
heson525 <heson525@163.com>
2021-01-07 16:05:40 +0800

    v0.1.4

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,7 @@
 import timeAgo from './timeAgo'
 import urlToLink from './urlToLink'
-import getData from './getData'
-import results from './results'
-
-
-
-
 
 export {
-    results,
     timeAgo,
-    urlToLink,
-    getData
+    urlToLink
 }
